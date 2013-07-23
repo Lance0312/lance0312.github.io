528614086c9dbfa55f2bb952439de89952e1d990
Lance Chen <cyen0312@gmail.com>
2013-07-23 15:19:33 +0800

    Migrate to pelican from octopress

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Lance'
+SITENAME = u"Lance's blog"
+SITEURL = ''
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = u'en'
+
+DEFAULT_CATEGORY = ''
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS =  (('', ''),)
+
+# Social widget
+#SOCIAL = (('', ''),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PLUGINS = ['pelican_gist',]
+
+MARKUP = ('mkd',)
+
+THEME = './themes/gum/'
+
+GITHUB_URL = 'https://github.com/Lance0312'
+TWITTER_URL = 'https://twitter.com/Lance0312'
+
+MENUITEMS = [('Archives','/archives.html'),]
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
